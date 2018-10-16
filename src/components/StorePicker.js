62e2e98ed7fa137c7b896f3930185baac9f41c22
clwilkening <clw13clw13@gmail.com>
2018-10-16 19:40:15 -0400

    add form submit, route change

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  
+  goToStore = (e) => {
+    e.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder='Store Name'/>
+        <input type="text" required defaultValue={getFunName()} ref={this.myInput}/>
         <button type="submit">Visit Store</button>
       </form>
     )
