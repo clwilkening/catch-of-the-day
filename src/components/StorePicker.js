5ee1ccfa0aef3bb3ee36bc49d97f7af4e628d357
clwilkening <clw13clw13@gmail.com>
2018-10-16 08:49:58 -0400

    link css, add store form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,13 @@ import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I'm da picker!</p>
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder='Store Name'/>
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
