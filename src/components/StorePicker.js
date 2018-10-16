25b63a2a942690b33b13317164a4d244f2897a13
clwilkening <clw13clw13@gmail.com>
2018-10-16 07:56:01 -0400

    create store picker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>I'm da picker!</p>
+  }
+}
+
+export default StorePicker;
