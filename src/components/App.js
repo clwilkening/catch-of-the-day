cbccde8670ca57d3c6eac11cd10033d38cb6f56b
clwilkening <clw13clw13@gmail.com>
2018-11-04 07:59:03 -0500

    add order to state

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,14 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   }
 
+  addToOrder = (key) => {
+    // take a copy of state
+    const order = {...this.state.order};
+    // add or update order
+    order[key] = order[key] + 1 || 1; 
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -28,7 +36,11 @@ class App extends React.Component {
           <Header tagline="Fresh Market"/>
           <ul className='fishes'>
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish 
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
